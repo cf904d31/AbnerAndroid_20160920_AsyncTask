7b5cd77170b09e32e31330d59e4fd0ffe77c2ab1
cf904d31 <cf904d31@gmail.com>
2016-09-20 10:32:14 +0800

    AsyncTask(按下按鈕show出圖片(從網路上抓取的))，回家將之與AsyncTask結合

--- app/src/main/java/iii/org/tw/asynctask/MainActivity.java
+++ app/src/main/java/iii/org/tw/asynctask/MainActivity.java
@@ -1,21 +1,36 @@
 package iii.org.tw.asynctask;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private TextView showText;
     private MyTask mt1;
+    private ImageView showImg;
+    private Bitmap bmp;
+    private UIHandler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showText = (TextView) findViewById(R.id.showText);
+        showImg = (ImageView) findViewById(R.id.showImg);
+        handler = new UIHandler();
     }
 
     private class MyTask extends AsyncTask<String,Object,String> {
@@ -75,6 +90,33 @@ public class MainActivity extends AppCompatActivity {
         if (mt1 != null && !mt1.isCancelled()) {
             //-----這裡通常給true值，代表不管後台有無東西在動作都會直接被中斷
             mt1.cancel(true);
+        }
+    }
+
+    public void showImgView(View v) {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://www.androidcentral.com/sites/androidcentral.com/files/styles/w700/public/postimages/%5Buid%5D/podcast-ac-new.jpg?itok=tKuELSVP");
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.connect();
+                    bmp = BitmapFactory.decodeStream(conn.getInputStream());
+                    handler.sendEmptyMessage(0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            showImg.setImageBitmap(bmp);
         }
     }
 }
